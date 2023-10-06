04e63eaa76b7b2ea220c7d9d467fc81cb67c4759
DiamondLionLV <rudolfs.rijkuris05@gmail.com>
2023-10-07 02:48:37 +0300

    linki salikti bro

--- webscraper/get_lesson_data.py
+++ webscraper/get_lesson_data.py
@@ -4,7 +4,19 @@ import time
 import json
 
 
-urls = []
+urls = [
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/Baseline-w/CHANGE-3/3/2020_12_29_nasa-hdbk-4007_w-change_3_revalidated.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/Baseline-w/CHANGE-1/1/Historical/2013-12-02_NASA-HDBK-4008_Draft1_Clean_Published.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/w/CHANGE-1/1/nasa-hdbk-4009a_w-chg_1.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/Baseline-w/CHANGE-1/1/nasa-hdbk-_5010_revalidated.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/Baseline-w/CHANGE-2/2/2022-12-16-NASA-HDBK-6024_w-Chg-2_Reval-Final.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/Baseline-w/CHANGE-1/1/nasa-hdbk-6025_w_change_1.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/Baseline-w/CHANGE-2/2/2021-07-16_nasa-spec-5022_w-change_2_revalidation_w-edit-admin_chgs_final.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/w/CHANGE-1/1/NASA-STD-4003A_w-Change-1-Revalidated.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/A/1/2021-11-17_NASA_STD_4005A_w-Change-1-Revalidation-Final.pdf",
+    "https://standards.nasa.gov/sites/default/files/standards/NASA/w/Change-1/1/nasa-std-4009a_w-chg_1.pdf",
+]
+
 with open("./webscraper/urls.json", "r") as json_file:
     urls = json.load(json_file)
 
