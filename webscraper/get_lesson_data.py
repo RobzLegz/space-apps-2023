cea8258b493ecdc0e12f681583e86308f0a7fcdb
RobzLegz <robzlegz@gmail.com>
2023-10-07 08:22:33 +0300

    data

--- webscraper/get_lesson_data.py
+++ webscraper/get_lesson_data.py
@@ -22,7 +22,9 @@ for url in urls:
 
         lesson_data = lesson.find_elements(By.TAG_NAME, "div")
 
-        obj = {}
+        obj = {
+            "url": url
+        }
 
         for d in lesson_data:
             try:
@@ -54,7 +56,7 @@ for url in urls:
                 print(e)
                 pass
 
-            if len(obj) > 1:
+            if len(obj) > 2:
                 print(obj)
                 print("")
                 data.append(obj)
