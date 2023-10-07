d15adef3a1ffa7559f4addda8cdec9d95dbcd97a
RobzLegz <robzlegz@gmail.com>
2023-10-07 18:10:54 +0300

    UI

--- frontend/src/components/home/HomeContainer.tsx
+++ frontend/src/components/home/HomeContainer.tsx
@@ -48,8 +48,8 @@ const HomeContainer = () => {
   };
 
   return (
-    <div className="w-full max-w-[1000px] flex flex-col my-10 p-4 rounded-lg bg-primary-800 border border-primary-700">
-      <h1 className="text-accent text-lg mb-3">D.V. Terminal</h1>
+    <div className="w-full max-w-[900px] flex flex-col my-10 p-4 rounded-lg bg-primary-800 border border-primary-700">
+      <h1 className="text-accent text-lg mb-3 font-semibold">D.V. Terminal</h1>
 
       <input
         type="file"
@@ -61,7 +61,9 @@ const HomeContainer = () => {
       />
 
       <label
-        className="w-full h-[370px] flex items-center justify-center border-2 border-dashed border-accent rounded-lg text-accent flex-col cursor-pointer"
+        className={`w-full h-[300px] flex items-center justify-center transition-all duration-200 border-2 rounded-lg text-accent flex-col cursor-pointer ${
+          dragActive ? "border-accent-100 border-solid" : "border-accent border-dashed"
+        }`}
         onDrag={handleDrag}
         onDragEnter={handleDrag}
         onDragOver={handleDrag}
@@ -76,32 +78,36 @@ const HomeContainer = () => {
         <small className="text-gray-400 mt-2">Or click to upload</small>
       </label>
 
-      <div className="flex w-full items-center justify-start gap-3">
-        {files.length > 0 && (
-          <div className="flex text-gray-400 gap-3 mt-2">
-            {files.map((file, i) => (
-              <p
-                key={i}
-                className="hover:line-through cursor-pointer"
-                onClick={() => setFiles(files.filter((f, j) => j !== i))}
-              >
-                {file.name}
-              </p>
-            ))}
+      {files.length > 0 && (
+        <div className="flex flex-col items-start justify-start mt-4 overflow-x-auto w-full whitespace-nowrap pb-2">
+          <p className="text-gray-400">Documents to validate:</p>
+
+          <div className="flex w-full items-center justify-start gap-3 mt-1">
+            <div className="flex text-accent gap-3 ">
+              {files.map((file, i) => (
+                <p
+                  key={i}
+                  className="hover:line-through cursor-pointer"
+                  onClick={() => setFiles(files.filter((f, j) => j !== i))}
+                >
+                  {file.name}
+                </p>
+              ))}
+            </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
 
       <textarea
         name="terminal"
         id="terminal"
-        className="w-full h-full mt-4 bg-primary-900 p-4 min-h-[90px] rounded-lg border border-primary-700"
+        className="w-full h-full mt-2 bg-primary-900 p-4 min-h-[90px] rounded-lg border border-primary-700"
         placeholder="Add context if needed..."
         rows={2}
       />
 
-      <div className="p-2 w-full mt-4 border-2 border-dashed border-primary-700 rounded-md flex justify-start items-start">
-        <button className="bg-accent rounded-md px-4 py-2 text-primary-900">
+      <div className="w-full mt-4 flex">
+        <button className="bg-accent rounded-md px-12 py-2 text-primary-900 border-accent-100 hover:bg-accent-100 duration-200 transition-colors">
           Submit
         </button>
       </div>
