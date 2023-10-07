e521bc99adb5a4a6d557e2c24eca293ef3ebc2d1
RobzLegz <robzlegz@gmail.com>
2023-10-07 22:30:48 +0300

    data split

--- backend/src/logic/engine/controller.ts
+++ backend/src/logic/engine/controller.ts
@@ -6,26 +6,26 @@ import multer from "multer";
 export const engineCtrl = {
   uploadPDF: async (_req: Request, res: Response) => {
     try {
-    //   let buffer = fs.readFileSync()
+      //   let buffer = fs.readFileSync()
       const storage = multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, "./");
+          cb(null, "./");
         },
         filename: (req, file, cb) => {
-            cb(null, `${Date.now()}-${file.originalname}`);
+          cb(null, `${Date.now()}-${file.originalname}`);
         },
       });
 
-      const upload = multer({storage}).single('file');
-      
+      const upload = multer({ storage }).single("file");
+
       upload(_req, res, async (err: any) => {
         if (err) {
-            console.error(err);
-            return res.status(500).json({ err: "Radās kļūme" })
+          console.error(err);
+          return res.status(500).json({ err: "Radās kļūme" });
         }
 
         if (!_req.file) {
-            return res.status(400).json({ err: "No file uploaded." })
+          return res.status(400).json({ err: "No file uploaded." });
         }
 
         const pdfFilePath = _req.file.path;
@@ -33,16 +33,22 @@ export const engineCtrl = {
         const pdfText = await pdf(pdfData);
 
         fs.unlink(pdfFilePath, (unlinkErr) => {
-            if (unlinkErr) {
-              console.error("Error deleting file:", unlinkErr);
-            } else {
-              console.log("File deleted successfully.");
-            }
+          if (unlinkErr) {
+            console.error("Error deleting file:", unlinkErr);
+          } else {
+            console.log("File deleted successfully.");
+          }
         });
 
+        const paragraphs = pdfText.text
+          .split(/\r?\n\r?\n/)
+          .filter((p) => p.replaceAll(" ", "").length);
+
+        console.log(paragraphs);
+
         return res.json({
-            msg: "File uploaded successfully",
-            pdfText: pdfText.text,
+          msg: "File uploaded successfully",
+          paragraphs,
         });
       });
     } catch (err: any) {
